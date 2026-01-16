d7cd097fa6bc43fa9fff1af7f79cb9c9b61e2bc8
vie_bcit <vienna_ly@bcit.ca>
2026-01-15 18:13:27 -0800

    3-way stopcock -- poc

---
+++ Assets/Scripts/Stopcock.cs
@@ -0,0 +1,47 @@
+/* Controller for game object that behaves like a stopcock
+    Specify number of ports angles
+    Which ports are active at each angle
+    Which hoses are connected at each port
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stopcock : MonoBehaviour
+{
+    [Tooltip("The stopcock game object to rotate")]
+    public GameObject stopcockObject;
+
+    [Tooltip("The angles (in degrees) for each stopcock position")]
+    public float[] anglePositions;
+
+    private int currentPositionIndex = 0;
+
+    private void Start()
+    {
+        UpdateStopcock();
+    }
+    private void OnMouseDown()
+    {
+        RotateToNextPosition();
+    }
+    private void RotateToNextPosition()
+    {
+        currentPositionIndex = (currentPositionIndex + 1) % anglePositions.Length;
+        UpdateStopcock();
+    }
+
+    private void UpdateStopcock()
+    {
+        // Rotate the stopcock to the correct angle
+        float targetAngle = anglePositions[currentPositionIndex];
+        stopcockObject.transform.localRotation = Quaternion.Euler(0, targetAngle, 0);
+
+    }
+
+    // Public method to get the current position index
+    public int GetCurrentPositionIndex()
+    {
+        return currentPositionIndex;
+    }
+}
