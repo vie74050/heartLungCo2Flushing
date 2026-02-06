d02544a45753c6e35cce25d8d7f38c4b260cbdf7
vie_bcit <vienna_ly@bcit.ca>
2026-02-05 16:10:07 -0800

    Add flow node clamp control - ERC monitor ON OFF buttons clamp/unclamp functionality

---
+++ Assets/Scripts/Interactible_ClickActions/FlowNodeClampControl.cs
@@ -0,0 +1,42 @@
+/* A ClickAction handler that sets the clamp state of a FlowNodeHose */
+using UnityEngine;
+
+public class FlowNodeClampControl : ClickAction
+{
+    [Tooltip("The target FlowNodeHose to set flow clamp on")]
+    public FlowNodeHose targetHose;
+
+    [Tooltip("The clamp object Transform reference")]
+    public Transform clampObjectRefTn;
+
+    void Start() {
+        // for linked ON OFF buttons, ensure they have the same parent 
+        if (clampObjectRefTn == null)
+            clampObjectRefTn = transform.parent; 
+    }
+    public override void OnClick()
+    {
+        if (targetHose == null)
+        {
+            Debug.LogWarning("FlowNodeClampControl: Missing targetHose reference.");
+            return;
+        }
+
+        switch (onClickActiveMode)
+        {
+            case GameObjectActiveMode.SetTrue:
+                targetHose.SetClamp(clampObjectRefTn.gameObject, true);
+                break;
+            case GameObjectActiveMode.SetFalse:
+                targetHose.SetClamp(clampObjectRefTn.gameObject, false);
+                break;
+            case GameObjectActiveMode.Toggle:
+                bool isClamped = targetHose.isClamped;
+                targetHose.SetClamp(clampObjectRefTn.gameObject, !isClamped);
+                break;
+        }
+
+    }
+
+    
+}
