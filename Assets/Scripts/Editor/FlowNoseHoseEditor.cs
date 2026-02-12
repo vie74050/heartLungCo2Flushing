ddbf3a5ef8a3fa873c5c6ac056cbf9b07eabf58d
vie_bcit <vienna_ly@bcit.ca>
2026-02-11 19:10:31 -0800

    2026-02-11 change request: - 3 WAY STOP COCK INLET LINE

--- Assets/Scripts/Editor/FlowNoseHoseEditor.cs
+++ Assets/Scripts/Editor/FlowNoseHoseEditor.cs
@@ -1,6 +1,6 @@
 using UnityEditor;
 using UnityEngine;
-/*
+
 #if UNITY_EDITOR
     [CustomEditor(typeof(FlowNodeHose))]
     public class FlowNodeHoseEditor : Editor
@@ -17,4 +17,3 @@ using UnityEngine;
         }
     }
 #endif
-*/
